3697b18dcfee403aad12deebb29e26a06116824b
suchu <suchu01@hotmail.com>
2020-12-11 14:59:22 +0800

    文件名特殊符号处理 HttpRequset设置默认超时时间:5s

--- src/main/java/online/githuboy/lagou/course/utils/FileUtils.java
+++ src/main/java/online/githuboy/lagou/course/utils/FileUtils.java
@@ -8,4 +8,16 @@ public class FileUtils {
     public static void save(byte[] bytes, File path) {
         FileUtil.writeBytes(bytes, path);
     }
+
+    public static String getCorrectFileName(String originFileName) {
+        return originFileName.replaceAll("[\\\\s/:*?\"<>|]",
+                "");
+    }
+
+    public static void main(String[] args) {
+        String str = "\\\\/////////////A:*B??:::\"<C\">||||";
+        String r = str.replaceAll("[\\\\s/:*?\"<>|]", "");
+        System.out.println(FileUtils.getCorrectFileName("01 | Spring Data JPA 初识"));
+        System.out.println(r);
+    }
 }
