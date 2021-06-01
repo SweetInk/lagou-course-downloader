c9310144500062a19cba45983f3fa2ea64cd6ce3
suchu <suchu01@hotmail.com>
2021-06-01 13:02:10 +0800

    feat 优先从kaiwu获取视频播放地址

--- src/main/java/online/githuboy/lagou/course/task/MP4Downloader.java
+++ src/main/java/online/githuboy/lagou/course/task/MP4Downloader.java
@@ -1,6 +1,7 @@
 package online.githuboy.lagou.course.task;
 
 import cn.hutool.core.io.StreamProgress;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -28,7 +29,10 @@ import static online.githuboy.lagou.course.decrypt.alibaba.AliPlayerDecrypt.getP
 @Slf4j
 public class MP4Downloader extends AbstractRetryTask implements NamedTask, MediaLoader {
     private static final String API_TEMPLATE = "https://gate.lagou.com/v1/neirong/kaiwu/getLessonPlayHistory?lessonId={0}&isVideo=true";
-
+    /**
+     * 0 -> videoId
+     */
+    private static final String PLAY_INFO_API = "https://gate.lagou.com/v1/neirong/kaiwu/getPlayInfo?lessonId=0&courseId=0&sectionId=0&vid={0}";
     private final static int maxRetryCount = 3;
     private final String videoName;
     private final String appId;
@@ -61,48 +65,61 @@ public class MP4Downloader extends AbstractRetryTask implements NamedTask, Media
         String body = HttpUtils.get(url, CookieStore.getCookie()).header("x-l-req-header", "{deviceType:1}").execute().body();
         JSONObject jsonObject = JSON.parseObject(body);
         if (jsonObject.getInteger("state") != 1) throw new RuntimeException("获取课程信息失败:" + body);
-        String aliPlayAuth = jsonObject.getJSONObject("content").getJSONObject("mediaPlayInfoVo").getString("aliPlayAuth");
         String fileId = jsonObject.getJSONObject("content").getJSONObject("mediaPlayInfoVo").getString("fileId");
-        String playInfoRequestUrl = getPlayInfoRequestUrl(aliPlayAuth, fileId);
-        String response = HttpRequest.get(playInfoRequestUrl).execute().body();
-        log.debug("\nAPI request result:\n\n" + response);
-        JSONObject mediaObj = JSON.parseObject(response);
-        if (mediaObj.getString("Code") != null) throw new RuntimeException("获取媒体信息失败:");
-        JSONObject playInfoList = mediaObj.getJSONObject("PlayInfoList");
-        JSONArray playInfos = playInfoList.getJSONArray("PlayInfo");
-        if (playInfos != null && playInfos.size() > 0) {
-            JSONObject playInfo = playInfos.getJSONObject(0);
-            String mp4Url = playInfo.getString("PlayURL");
-            log.info("解析出【{}】MP4播放地址:{}", videoName, mp4Url);
-
-            HttpRequest.get(mp4Url).execute(true).writeBody(new File(workDir, FileUtils.getCorrectFileName(videoName) + ".mp4"), new StreamProgress() {
-                @Override
-                public void start() {
-                    log.info("开始下载视频【{}】lessonId={}", videoName, lessonId);
-                    if (startTime == 0) {
-                        startTime = System.currentTimeMillis();
-                    }
-                }
-
-                @Override
-                public void progress(long l) {
-                }
-
-                @Override
-                public void finish() {
-                    Stats.remove(videoName);
-                    Mp4History.append(lessonId);
-                    long count = latch.getCount();
-                    log.info("====>视频下载完成【{}】,耗时:{} s，剩余{}", videoName, (System.currentTimeMillis() - startTime) / 1000, count - 1);
-                    latch.countDown();
-                }
-            });
-
-        } else {
-            log.warn("没有获取到视频【{}】播放地址:", videoName);
-            latch.countDown();
+        //优先从拉钩视频平台获取可直接播放的URL
+        String playUrl = tryGetPlayUrlFromKaiwu(fileId);
+        if (StrUtil.isBlank(playUrl)) {
+            String aliPlayAuth = jsonObject.getJSONObject("content").getJSONObject("mediaPlayInfoVo").getString("aliPlayAuth");
+            String playInfoRequestUrl = getPlayInfoRequestUrl(aliPlayAuth, fileId);
+            String response = HttpRequest.get(playInfoRequestUrl).execute().body();
+            log.debug("\nAPI request result:\n\n" + response);
+            JSONObject mediaObj = JSON.parseObject(response);
+            if (mediaObj.getString("Code") != null) throw new RuntimeException("获取媒体信息失败:");
+            JSONObject playInfoList = mediaObj.getJSONObject("PlayInfoList");
+            JSONArray playInfos = playInfoList.getJSONArray("PlayInfo");
+            if (playInfos != null && playInfos.size() > 0) {
+                JSONObject playInfo = playInfos.getJSONObject(0);
+                playUrl = playInfo.getString("PlayURL");
+                log.info("解析出【{}】MP4播放地址:{}", videoName, playUrl);
+            } else {
+                log.warn("没有获取到视频【{}】播放地址:", videoName);
+                latch.countDown();
+                return;
+            }
         }
+        HttpRequest.get(playUrl).execute(true).writeBody(new File(workDir, FileUtils.getCorrectFileName(videoName) + ".mp4"), new StreamProgress() {
+            @Override
+            public void start() {
+                log.info("开始下载视频【{}】lessonId={}", videoName, lessonId);
+                if (startTime == 0) {
+                    startTime = System.currentTimeMillis();
+                }
+            }
 
+            @Override
+            public void progress(long l) {
+            }
+
+            @Override
+            public void finish() {
+                Stats.remove(videoName);
+                Mp4History.append(lessonId);
+                long count = latch.getCount();
+                log.info("====>视频下载完成【{}】,耗时:{} s，剩余{}", videoName, (System.currentTimeMillis() - startTime) / 1000, count - 1);
+                latch.countDown();
+            }
+        });
+    }
+
+    private String tryGetPlayUrlFromKaiwu(String fileId) {
+        String url = MessageFormat.format(PLAY_INFO_API, fileId);
+        String body = HttpUtils.get(url, CookieStore.getCookie()).header("x-l-req-header", "{deviceType:1}").execute().body();
+        JSONObject jsonObject = JSON.parseObject(body);
+        if (jsonObject.getInteger("state") != 1) {
+            log.error("获取mp4播放地址失败:{}", body);
+            return null;
+        }
+        return jsonObject.getJSONObject("content").getString("playURL");
     }
 
     @Override
@@ -120,7 +137,7 @@ public class MP4Downloader extends AbstractRetryTask implements NamedTask, Media
         try {
             Thread.sleep(200);
         } catch (InterruptedException e1) {
-            log.error("{}", e1);
+            log.error("线程休眠异常", e1);
         }
         ExecutorService.execute(this);
     }
