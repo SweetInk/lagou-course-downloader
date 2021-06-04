7e9f9a0d8e3d28fc2b00541fb538781d4890bea0
suchu <suchu01@hotmail.com>
2021-06-04 16:06:24 +0800

    doc 增加注释

--- src/main/java/online/githuboy/lagou/course/support/AbstractRetryTask.java
+++ src/main/java/online/githuboy/lagou/course/support/AbstractRetryTask.java
@@ -1,15 +1,25 @@
 package online.githuboy.lagou.course.support;
 
 /**
+ * 抽象重试任务
+ *
  * @author suchu
  * @date 2021/6/1
  */
 public abstract class AbstractRetryTask implements Retryable {
     protected int retryCNT = 0;
 
+    /**
+     * 这里写具体的业务逻辑
+     */
     protected void action() {
     }
 
+    /**
+     * 具体重试逻辑
+     *
+     * @param throwable 任务执行时抛出的异常
+     */
     protected void retry(Throwable throwable) {
         retryCNT++;
     }
--- src/main/java/online/githuboy/lagou/course/support/Retryable.java
+++ src/main/java/online/githuboy/lagou/course/support/Retryable.java
@@ -1,13 +1,23 @@
 package online.githuboy.lagou.course.support;
 
 /**
+ * 表示是否可重试
+ *
  * @author suchu
  * @date 2021/6/1
  */
 public interface Retryable extends Runnable {
 
+    /**
+     * 是否可已重试
+     *
+     * @return true 可以重试，false不能重试
+     */
     boolean canRetry();
 
+    /**
+     * 重试完成
+     */
     void retryComplete();
 
 }
